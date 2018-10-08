4188406ae7165dff064a8524fa0db2576da665c4
George Brande <george.brande@gmail.com>
2018-10-08 15:40:16 +0300

    al 2-lea spring rest api adaugat

---
+++ spring-postgres-rest/src/main/java/com/example/postgres/PostgresApplication.java
@@ -0,0 +1,15 @@
+package com.example.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PostgresApplication {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run(PostgresApplication.class, args);
+	}
+}
---
+++ spring-postgres-rest/src/main/java/com/example/postgres/controllers/AnswerController.java
@@ -0,0 +1,55 @@
+package com.example.postgres.controllers;
+
+import com.example.postgres.models.Answer;
+import com.example.postgres.repo.AnswerRepository;
+import com.example.postgres.repo.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class AnswerController {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping("/questions/{questionId}/answers")
+    public List<Answer>  getAnswersByQuestionsId(@PathVariable Long questionId) {
+        return answerRepository.findByQuestionId(questionId);
+    }
+
+    @PostMapping("/questions/{questionId}/answers")
+    public Answer addAnswer(@PathVariable Long questionId,
+                            @Valid @RequestBody Answer answer) {
+        return questionRepository.findById(questionId)
+                .map(
+                        question -> {
+                            answer.setQuestion(question);
+                            return answerRepository.save(answer);
+                        }
+                ).orElseThrow(() ->new NullPointerException());
+    }
+
+    @DeleteMapping("/questions/{questionId}/answers/{answerId}")
+    public ResponseEntity<?> deleteAnswer(@PathVariable Long questionId,
+                                          @PathVariable Long answerId) {
+        if(!questionRepository.existsById(questionId)) {
+            throw new NullPointerException("nu e boss");
+        }
+
+        return answerRepository.findById(answerId)
+                .map(answer -> {
+                    answerRepository.delete(answer);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new NullPointerException("nu e"));
+    }
+
+
+
+}
