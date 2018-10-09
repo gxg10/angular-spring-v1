6aa898ed0531699c0c9b283a48392b228a73cda4
George Brande <george.brande@gmail.com>
2018-10-09 16:39:59 +0300

    2.0

---
+++ frontend-angular/src/app/answer-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Answer } from './answer-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AnswerService {
+
+    apiUrl = 'http://localhost:8080/questions';
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getAnswers(questionId: number) {
+        return this.http.get(`${this.apiUrl}/${questionId}/answers`)
+    }
+}
--- frontend-angular/src/app/question-details/question-details.component.ts
+++ frontend-angular/src/app/question-details/question-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Question } from '../question-model';
 import { PackageServiceService } from '../package-service.service';
+import { AnswerService } from '../answer-service';
+import { Answer } from '../answer-model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-question-details',
@@ -10,10 +13,13 @@ import { PackageServiceService } from '../package-service.service';
 export class QuestionDetailsComponent implements OnInit {
 
   @Input() question: Question;
+  answer: Observable<Answer>;
 
-  constructor(private service: PackageServiceService) { }
+  constructor(private service: PackageServiceService,
+              private answerService: AnswerService) { }
 
   ngOnInit() {
+    this.getAns();
   }
 
   deleteQuestion() {
@@ -23,6 +29,15 @@ export class QuestionDetailsComponent implements OnInit {
           console.log(data);
         }
       );
+  }
+
+  getAns() {
+    this.answerService.getAnswers(this.question.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.answer = data;
+      }
+    );
   }
 
 }
