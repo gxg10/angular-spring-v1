0de26e5cb4e69823da5e276d04e0ab499c17abc1
George Brande <george.brande@gmail.com>
2018-10-10 16:24:10 +0300

    upload 2.0

--- spring-postgres-rest/src/main/java/com/example/postgres/PostgresApplication.java
+++ spring-postgres-rest/src/main/java/com/example/postgres/PostgresApplication.java
@@ -1,20 +1,31 @@
 package com.example.postgres;
 
+import com.example.postgres.services.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.Resource;
+
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-        FileStorageProperties.class
-})
 @EnableJpaAuditing
-public class PostgresApplication {
+public class PostgresApplication implements CommandLineRunner {
+
+    @Resource
+    StorageService storageService;
 
 	public static void main(String[] args) {
 
 	    SpringApplication.run(PostgresApplication.class, args);
 	}
+
+	@Override
+    public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+    }
+
 }
